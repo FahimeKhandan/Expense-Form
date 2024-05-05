1537b9052d0aaf840a04ea3742740255c855adfb
fahimekhandan <fahimekhandan@gmail.com>
2024-05-05 14:45:56 +0330

    feat: added form filter and expense table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpensesFilter from "./components/ExpensesFilter";
+import ExpenseForm from "./components/ExpenseForm";
+
+export const categories = ["Utilities", "Groceries", "Entertainment"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 2, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 2, category: "Groceries" },
+    { id: 3, description: "ccc", amount: 2, category: "Entertainment" },
+  ]);
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const onDelete = (id: number) => {
+    const newExpenses = expenses.filter((expense) => expense.id !== id);
+
+    setExpenses(newExpenses);
+  };
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="mb-2">
+          <ExpensesFilter
+            onSelectCategory={(category) => setSelectedCategory(category)}
+          />
+        </div>
+        <ExpenseList
+          expenses={visibleExpenses}
+          onDelete={(id) => onDelete(id)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,60 @@
+interface Expense {
+  id: number;
+  description: string;
+  category: string;
+  amount: number;
+}
+
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
+  if (expenses.length === 0)  return null;
+
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Descriptiopn</th>
+          <th>Amount</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            $
+            {expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ExpenseList;
